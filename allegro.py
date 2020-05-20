96876d02a8b4ada087c9797db09205d5cdfa096c
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-20 21:16:37 +0200

    Added other offers from site

--- allegro.py
+++ allegro.py
@@ -17,14 +17,36 @@ class Allegro:
     def best_offers(self):
         print('MEGA OFERTY')
         print('---')
-        for best_offers in self.get_all_best_offers().find_all('div', {
+        for best_offer in self.get_all_best_offers().find_all('div', {
             'class': '_3kk7b _07bcb_CWOtz _vnd3k _1h8s6 _n1rmb _1t6t8 _m44ca _07bcb_3q-O5'}):
-            best_offers_name = best_offers.find('a', {'class': '_1h7wt _15mod _07bcb_2W89U'}).text
-            print(best_offers_name)
-            best_offers_first_price = best_offers.find('span', {"class": "_swyoj _07bcb_1sAFC"}).text
-            best_offers_second_price = best_offers.find('span', {"class": "_1svub _lf05o"}).text
-            print(f'{best_offers_first_price} ---> {best_offers_second_price}')
+            best_offer_name = best_offer.find('a', {'class': '_1h7wt _15mod _07bcb_2W89U'}).text
+            print(best_offer_name)
+            best_offer_first_price = best_offer.find('span', {"class": "_swyoj _07bcb_1sAFC"}).text
+            best_offer_second_price = best_offer.find('span', {"class": "_1svub _lf05o"}).text
+            print(f'{best_offer_first_price} ---> {best_offer_second_price}')
+
         print('---')
+
+    def category_offers(self):
+        other_offersx = self.soup.find('div', {'data-box-name': 'MMO_KONTENER_KATEGORIE'})
+
+        for other_offers in other_offersx.find_all('div', {
+            'class': 'opbox-sheet-wrapper _7qjq4 _1yhvf _7ccvy _26e29_2AYAm _9huvz'})[1:]:
+            category_name = other_offers.find('h2', {
+                'class': 'container-header _1s2v1 _n2pii _35enf _sdhee _9f0v0 _g1a3g _1bwbg _1pelm'})
+            print(category_name.text)
+            print('---')
+            for offer in other_offers.find_all('div', {
+                'class': '_3kk7b _07bcb_CWOtz _vnd3k _1h8s6 _n1rmb _1t6t8 _m44ca _07bcb_3q-O5'}):
+                offer_name = offer.find('a', {'class': '_1h7wt _15mod _07bcb_2W89U'}).text
+                print(offer_name)
+                try:
+                    offer_first_price = offer.find('span', {"class": "_swyoj _07bcb_1sAFC"}).text
+                    offer_second_price = offer.find('span', {"class": "_1svub _lf05o"}).text
+                except AttributeError as e:
+                    offer_first_price = 'Brak promocji'
+                    offer_second_price = 'Nowość!'
+                print(f'{offer_first_price} ---> {offer_second_price}')
 
 
 if __name__ == '__main__':
@@ -32,3 +54,4 @@ if __name__ == '__main__':
 
     allegro = Allegro(source)
     allegro.best_offers()
+    allegro.category_offers()
