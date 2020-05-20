58b0113fcf6d42b49ee2ea54136bb8f83723e8fa
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-20 19:11:22 +0200

    Added best offers from site

--- allegro.py
+++ allegro.py
@@ -1,6 +1,34 @@
 from bs4 import BeautifulSoup
 import requests
 
-source = requests.get('https://allegro.pl/strefaokazji').text
 
-soup = BeautifulSoup(source, 'lxml')
+class Allegro:
+
+    def __init__(self, site):
+        self.site = site
+        self.soup = self.get_all_html()
+
+    def get_all_html(self):
+        return BeautifulSoup(self.site, 'lxml')
+
+    def get_all_best_offers(self):
+        return self.soup.find('div', {"data-box-name": "MMO_ADVERT"})
+
+    def best_offers(self):
+        print('MEGA OFERTY')
+        print('---')
+        for best_offers in self.get_all_best_offers().find_all('div', {
+            'class': '_3kk7b _07bcb_CWOtz _vnd3k _1h8s6 _n1rmb _1t6t8 _m44ca _07bcb_3q-O5'}):
+            best_offers_name = best_offers.find('a', {'class': '_1h7wt _15mod _07bcb_2W89U'}).text
+            print(best_offers_name)
+            best_offers_first_price = best_offers.find('span', {"class": "_swyoj _07bcb_1sAFC"}).text
+            best_offers_second_price = best_offers.find('span', {"class": "_1svub _lf05o"}).text
+            print(f'{best_offers_first_price} ---> {best_offers_second_price}')
+        print('---')
+
+
+if __name__ == '__main__':
+    source = requests.get('https://allegro.pl/strefaokazji').text
+
+    allegro = Allegro(source)
+    allegro.best_offers()
