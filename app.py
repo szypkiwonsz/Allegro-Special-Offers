a8a8c736a151b5667aed2dbbd050e0fef00e5321
szypkiwonsz <kacpersawicki321@gmail.com>
2020-05-24 19:48:42 +0200

    Added sorting elements by the best offer

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     source = requests.get('https://allegro.pl/strefaokazji').text
-    best_offers = BestOffers(source)
-    best_offers = best_offers.best_offers()
+    best_offers_products = BestOffers(source)
+    best_offers = sorted(best_offers_products.best_offers(), key=lambda x: int((x[4]).replace('%', '')), reverse=True)
     return render_template('index.html', **locals())
 
 
